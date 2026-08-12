9ff5c25c668862b008446597a8d4674ee14ef097
Heather Noonan <65779487+noonanhe-bot@users.noreply.github.com>
2026-08-12 19:49:18 -0400

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.cpp
@@ -0,0 +1,37 @@
+class LRUCache {
+private:
+    unordered_map<int, pair<int, list<int>::iterator>> cache_;
+    list<int> order;
+    int capacity_;
+public:
+    LRUCache(int capacity) {
+        capacity_ = capacity;
+    }
+    
+    int get(int key) {
+        if (cache_.contains(key)) {
+            // if we find the key we need to update it's position in the list
+            // by first erasing it's position in the list
+            order.erase(cache_[key].second);
+            order.push_back(key); // then pushing the key to the back
+            cache_[key].second = --order.end(); // and updating the pointer
+            return cache_[key].first;
+        } else {
+            return -1;
+        }
+    }
+    
+    void put(int key, int value) {
+        if (cache_.contains(key)) {
+            // if we find the key we need to update it's position in the list
+            // by first erasing it's position in the list
+            order.erase(cache_[key].second);
+        }else if (cache_.size() == capacity_) {
+            int lru = order.front();
+            cache_.erase(lru);
+            order.pop_front();
+        } 
+        order.push_back(key);
+        cache_[key] = {value, --order.end()};
+    }
+};
